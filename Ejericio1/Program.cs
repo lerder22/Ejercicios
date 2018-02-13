b445ce91651a7c88e80e0340556b079b10013502
lerder22 <alexlrdr@hotmail.com>
2018-02-13 19:03:39 -0300

    First commit

---
+++ Ejericio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejericio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1,b=2,c=3;
+            int d = a + b + c;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
